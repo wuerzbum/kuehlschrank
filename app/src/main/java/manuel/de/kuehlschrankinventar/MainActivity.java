1d5ab0b66f7f2209defc85071a4bacfcf432decc
Manuel <apps.manuel@gmx.de>
2020-10-15 14:41:21 +0200

    First Commit

---
+++ app/src/main/java/manuel/de/kuehlschrankinventar/MainActivity.java
@@ -0,0 +1,14 @@
+package manuel.de.kuehlschrankinventar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
