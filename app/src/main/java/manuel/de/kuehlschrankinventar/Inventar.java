a47d5f729c5418eb239d94ae72ab5e642d446c0f
Manuel <apps.manuel@gmx.de>
2020-10-15 16:05:51 +0200

    Inventar Klasse erstellt -> final String erstellt

--- app/src/main/java/manuel/de/kuehlschrankinventar/Inventar.java
+++ app/src/main/java/manuel/de/kuehlschrankinventar/Inventar.java
@@ -3,7 +3,6 @@ package manuel.de.kuehlschrankinventar;
 class Inventar {
     //Hallo Test;
     int a;
-   int b = 5;
-    final static String INVENTAR = "inventar";
-    int a;
+    int b = 5;
+    final static String INVENTAR = "inventar", ZWEIT_INVENTAR = "2. inventar";
 }
