e3f3f7d4f934d271b8c27f35f653dd68f9f44789
Manuel <apps.manuel@gmx.de>
2020-10-26 09:13:59 +0100

    master mit Updates

--- app/src/main/java/manuel/de/kuehlschrankinventar/activity/MainActivity.java
+++ app/src/main/java/manuel/de/kuehlschrankinventar/activity/MainActivity.java
@@ -1,12 +1,17 @@
 package manuel.de.kuehlschrankinventar.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-import manuel.de.kuehlschrankinventar.holder.Inventar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import manuel.de.kuehlschrankinventar.R;
+import manuel.de.kuehlschrankinventar.holder.Inventar;
+import manuel.de.kuehlschrankinventar.scanner.ScannedCodeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +31,37 @@ public class MainActivity extends AppCompatActivity {
             String abcFromR = getString(R.string.app_name);
             testTV.setText(abcFromR);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        /* Menu Icons in Dropdown anzeigen
+         * if (menu instanceof MenuBuilder) {
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+         }
+         */
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            //TODO falls doch gewünscht, dass man fotografieren kann das folgende wieder "entkommentieren"
+            /*case R.id.takePicture:
+                startActivity(new Intent(MainActivity.this, PictureCodeActivity.class));
+                break;*/
+
+            case R.id.scanBarcode:
+                Intent i = new Intent(MainActivity.this, ScannedCodeActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
