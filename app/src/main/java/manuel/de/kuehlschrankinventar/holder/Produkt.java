7d496f507e05ef000eed935c313f89ec1309f674
10RG <robin.geiser@web.de>
2020-11-20 09:38:27 +0100

    Klasse Produkt Methoden erstellt

--- app/src/main/java/manuel/de/kuehlschrankinventar/holder/Produkt.java
+++ app/src/main/java/manuel/de/kuehlschrankinventar/holder/Produkt.java
@@ -10,27 +10,35 @@ import java.util.TreeMap;
 public class Produkt {
 
     private String name;
-    //TODO eine Liste von Barcodes, dass einem Produkt mehrere Barcodes zugewiesen werden können?
-    //String, weil es meist 13 Stellige Zahlen sind, die nicht als integer gespeichert werden können
     private double menge;
     private Date verfallsdatum;
     private double preis;
-    private TreeMap<String, Double> besitzer;
-    private TreeMap<Benutzer, Double>minBestand;
+    private TreeMap<String, Double> besitzermenge;
+    private TreeMap<String, Double>minBestand; // TODO Treemap<Benutzer in TreeMap<Atring geändert
     private String barcode;
     private String einheit;
 
 
-
     /**
      * Neues Produkt erstellen
-     * @param name Name des Produktes
-     * @param barcode Barcode des Produktes
+     * @param name Produktname
+     * @param menge Menge
+     * @param verfallsdatum Verfallsdatum
+     * @param preis Preis
+     * @param besitzer Besitzer
+     * @param minBestand mindest Bestand
+     * @param barcode Barcodenummer
+     * @param einheit Einheit
      */
-    public Produkt(String name, double cgDouble1, Date cgDate, double cgDouble2, TreeMap <String, Double>cgTreeMap, TreeMap <Benutzer, Double> minBestand, String barcode, String einheit) {
-
-        this.name = name;
-        this.barcode = barcode;
+     public Produkt(String name, double menge, Date verfallsdatum, double preis, TreeMap <String, Double>besitzer, TreeMap <String, Double> minBestand, String barcode, String einheit) {
+        setName(name);
+        setMenge(menge);
+        setVerfallsdatum(verfallsdatum);
+        setPreis(preis);
+        setBesitzer(besitzer);
+        setMinBestand(minBestand);
+        setBarcode(barcode);
+        setEinheit(einheit);
     }
 
     public Produkt(String jsonString) {
@@ -38,27 +46,177 @@ public class Produkt {
     }
 
     /**
-     * Neuen Namen setzen
-     * @param newName neuer Produktname
+     * Neuen Produktnamen erstellen
+     * @param newName neuer Name
+     * @return True bei erfolgreicher Übergabe
      */
-    public void setName(String newName) {
+    public boolean setName( String newName) {
+        if (newName == null)
+        {
+            return false;
+        }else{
         this.name = newName;
+        return true;}
     }
 
     /**
-     * Neuen Barcode setzen
-     * @param newBarcode neuer Barcode
+     * Menge ändern
+     * @param newMenge Menge
+     * @return True bei erfolgreicher Übergabe
      */
-    public void setBarcode(String newBarcode) {
-        this.barcode = newBarcode;
+    public boolean setMenge(double newMenge){
+        if (newMenge < 0.0)
+        {
+            return false;
+        }else{
+            this.menge = newMenge;
+            return true;}
     }
 
+    /**
+     * Verfallsdatum eingeben
+     * @param newVerfallsdatum neues Verfallsdatum
+     * @return True bei erfolgreicher übergabe
+     */
+    public boolean setVerfallsdatum(Date newVerfallsdatum) {
+        if (newVerfallsdatum == null) {
+            return false;
+        } else {
+            this.verfallsdatum = verfallsdatum;
+            return true;
+        }
+    }
+
+    /**
+     * Preis ändern
+     * @param newPreis neuer Preis
+     * @return True bei erfolgreicher Übergabe
+     */
+    public boolean setPreis(double newPreis){
+        if (newPreis < 0.0)
+        {
+            return false;
+        }else{
+            this.menge = newPreis;
+            return true;}
+    }
+
+    /**
+     * neuer Besitzer
+     * @param newbesitzer neuer Besitzer
+     */
+    public void setBesitzer(TreeMap<String, Double> newbesitzer){
+        this.besitzermenge = newbesitzer;
+    }
+
+    /**
+     * Mindest Bestand ändern
+     * @param newMinBestand neuer mindest Bestand
+     */
+    public void setMinBestand(TreeMap<String, Double> newMinBestand){
+        this.minBestand = newMinBestand;
+    }
+
+    /**
+     * neuen Barcode erstellen
+     * @param newBarcode neuer barcode
+     * @return True bei erfolgreicher Übergabe
+     */
+    public boolean setBarcode(String newBarcode) {
+        if (newBarcode == null){
+            return  false;
+        }else{
+            this.barcode = newBarcode;
+            return true;
+        }
+    }
+
+    /**
+     * Einheit ändern
+     * @param newEinheit neue Einheit
+     * @return True bei erfolgreicher übergabe
+     */
+    public boolean setEinheit(String newEinheit) {
+        if (newEinheit == null){
+            return  false;
+        }else{
+            this.einheit = newEinheit;
+            return true;
+        }
+    }
     /**
      * Name des Produkts erhalten
      * @return gibt den Namen des Produktes zurück
      */
     public String getName() {
         return name;
+    }
+
+    public double getMenge() {
+        return menge;
+    }
+
+    public Date getVerfallsdatum() {
+        return verfallsdatum;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public double getBestandvonBesitzer(String name) {
+        if (besitzermenge != null && besitzermenge.containsKey(name)){
+        return besitzermenge.get(name);
+        }else {return 0.0;}
+    }
+
+    public double getMinBestand(String name) {
+        if (minBestand != null && minBestand.containsKey(name)){
+            return minBestand.get(name);
+        }else {return 0.0;}
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    /**
+     * Geringer Bestand melden
+     * @param name Benutzer
+     * @return True bei zu geringem Bestand
+     */
+    public boolean geringerBestand(String name) {
+        if (getMinBestand(name) < getBestandvonBesitzer(name))
+        {
+            return true;
+        } else {return false;}
+
+    }
+/*    //TODO wird nicht benötigt Getter Besitzemenge macht das schon ?????
+    public double bestandVonBesitzer(String name){
+        return 0.0;
+    }
+*/
+    public boolean neuerEinkauf(){
+       setMenge(this.menge + 1.0);
+        //TODO: Methode neuerEinauf in Klasse Produkt ausarbeiten
+        return false;
+    }
+
+    public boolean verbrauchen(){
+      if (this.menge != 0){
+        setMenge(this.menge - 1.0);
+      return true;
+      }else {return false;}
+    }
+
+    public void nutzerBenachrichtigen(){
+
+        //TODO: Methode nutzerBenachrichtigen in Klasse Produkt ausarbeiten (Notification)
     }
 
     /**
@@ -80,11 +238,6 @@ public class Produkt {
     public boolean hatBarcode() {
         return !barcode.equals("");
     }
-
-    public String getBarcode() {
-        return barcode;
-    }
-
     /**
      * Barcode vom Produkt entfernen
      * @param removeBarcode setzt den Barcode des Produktes auf ""
@@ -113,26 +266,5 @@ public class Produkt {
         }
     }
 
-    public void geringerBestand(){
-        //TODO: Methode geringerBestand in Klasse Produkt ausarbeiten
-    }
 
-    public double bestandVonBesitzer(String cgString){
-        //TODO: Methode bestandVonBesitzer in Klasse Produkt ausarbeiten
-        return 0.0;
-    }
-
-    public boolean neuerEinkauf(){
-        //TODO: Methode neuerEinauf in Klasse Produkt ausarbeiten
-        return false;
-    }
-
-    public boolean verbrauchen(){
-        //TODO: Methode verbrauchen in Klasse Produkt ausarbeiten
-        return false;
-    }
-
-    public void nutzerBenachrichtigen(){
-        //TODO: Methode nutzerBenachrichtigen in Klasse Produkt ausarbeiten
-    }
 }
